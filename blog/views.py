4e57aff9df329164fb240baa75bf9d04615922c3
Ekbal41 <mdasifekbalshagor@gmail.com>
2022-12-08 23:57:07 +0600

    init

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+def blog_detail(request):
+    return render(request, 'blog/blog-detail.html')
